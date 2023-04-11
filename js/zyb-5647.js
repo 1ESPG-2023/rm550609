2a7be7d9794451d3be8b63d8afcd44552198095f
victoriafpizza <rm550609@fiap.com.br>
2023-04-11 11:37:00 -0300

    salvando aq

---
+++ js/zyb-5647.js
@@ -0,0 +1,8 @@
+const liElement = document.getElementsByTagName("li");
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(liElement.lenght);
