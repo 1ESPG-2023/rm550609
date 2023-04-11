60c583fa96338b675c3b3c2ec4846d900c7a217e
victoriafpizza <rm550609@fiap.com.br>
2023-04-11 11:45:11 -0300

    cansei de dar commit

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,8 +1,17 @@
 const liElement = document.getElementsByTagName("li");
 
+il
+
 for (let index = 0; index < array.length; index++) {
     const element = array[index]
     
 }
 
 console.log(liElement.lenght);
+
+Let nr1= [1,2,3,4,5];
+Let nr2= [6,7,8,9,10];
+Let nr3 = nr1 + nr2;
+
+console.log(`Array- 1 $ {nr1}`);
+console.log(`Array- 2 $ {nr1}`);
