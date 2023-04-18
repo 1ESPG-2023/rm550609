ccda3a2842d69e6bb3dfe0d84258d0996ed305c3
victoriafpizza <rm550609@fiap.com.br>
2023-04-18 10:55:33 -0300

    ta tudo salvo

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -3,15 +3,15 @@ const liElement = document.getElementsByTagName("li");
 il
 
 for (let index = 0; index < array.length; index++) {
-    const element = array[index]
+    const element = array[index];
     
 }
 
 console.log(liElement.lenght);
 
-Let nr1= [1,2,3,4,5];
-Let nr2= [6,7,8,9,10];
-Let nr3 = nr1 + nr2;
+let nr1= [1,2,3,4,5];
+let nr2= [6,7,8,9,10];
+let nr3 = nr1 + nr2;
 
 console.log(`Array- 1 $ {nr1}`);
 console.log(`Array- 2 $ {nr1}`);
@@ -19,8 +19,25 @@ console.log(`Array- 2 $ {nr1}`);
 //Listando um array 
 
 nr3.forEach((iemdoArray)=>{
-
-console.log(`ARRAY SENDO LISTADO $ {nR3}`);
+    console.log(`ARRAY SENDO LISTADO $ {itemDoArray}`);
 
 })
-.
+
+// listando array com map
+    nr3.map((numero,key)=>{
+        console.log(`${(key + 1)} itens do array - ${numero} `);
+        numero.forEach((digitos)=>{
+
+        })
+    })
+    
+    const liElementHtml = document.getElementsByTagName("li");
+    const liElementArray = [...liElementHtml];
+
+    liElement.map((item,key)=>{
+        if (item.textContent == "Item-39") {
+        console.log(`${key+1} - ${item.textContent = "0"}`)
+        }
+    })
+
+ 
