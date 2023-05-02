e24a8b3d313e53c2d68d9d2630d57aec049f7fe2
victoriafpizza <rm550609@fiap.com.br>
2023-05-02 10:44:06 -0300

    salvando corretamente

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,52 +1,72 @@
-const liElement = document.getElementsByTagName("li");
 
-il
+  
+// // for (let index = 0; index < liElement.length ; index++) {
+// //     console.log(liElement[index].textContent)
+// // }
+// // // console.log(liElement.length)
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // // let nr3 = nr1+nr2;
+// // let nr3 = [nr1,nr2];
+
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
+
+// // //Listando um array! Técnica especial
+// // // nr3.forEach((itemDoaArray)=>{
+// // //     console.log(`Array-3 sendo listado ${itemDoaArray}`);
+// // // })
+// // // Listando o array com MAP
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+// //     numero.forEach((digitos)=>{
+// //         console.log(digitos)
+// //     })
+// // })
+
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // //Espalhar o array com SPREAD [ ... ]
+// // let nr3 = [...nr1,...nr2];
+
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
+
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+
+// // })
+
+// //Convertendo HTMLCollection em Array de forma indireta
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml];
+
+// liElementArray.map((item,key)=>{
+//     if (item.textContent == "Item-39") {
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
+// })
+
+function soma(p1,p2,p3) {
+    nr1 = p1;
+    nr2 = p2;
+    resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
+    console.log(`O Resultado é : ${resultado}.`)
+    return (resultado)
+    // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+    // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
 }
 
-console.log(liElement.lenght);
-
-let nr1= [1,2,3,4,5];
-let nr2= [6,7,8,9,10];
-let nr3 = nr1 + nr2;
-
-console.log(`Array- 1 $ {nr1}`);
-console.log(`Array- 2 $ {nr1}`);
-
-//Listando um array 
-
-nr3.forEach((iemdoArray)=>{
-    console.log(`ARRAY SENDO LISTADO $ {itemDoArray}`);
-
-})
-
-// listando array com map
-    nr3.map((numero,key)=>{
-        console.log(`${(key + 1)} itens do arrayyy - ${numero} `);
-        numero.forEach((digitos)=>{
-
-        })
-    })
+//ARROW FUNCTION
+const mudaCor = ()=>{
     
-    const liElementHtml = document.getElementsByTagName("li");
-    const liElementArray = [...liElementHtml];
+    //FUNÇÃO SET-TIME-OUT
+    setTimeout(soma, 5000, 100,100,100);
+}
 
-    liElement.map((item,key)=>{
-        if (item.textContent == "Item-39") {
-        console.log(`${key+1} - ${item.textContent = "localizado"}`)
-        }
-    })
-
-    function soma(){
-        nr1=10;
-        nr2=5;
-        console.log(`resultado : ${nr1 + nr2}`);
-        console.log(`ESTE É O PARAMETRO QUE FOI PASSADO PARA A FUNÇÃO: $(parametro)`)
-    }
-
-    soma();
-
- 
+mudaCor()
